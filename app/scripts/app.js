e2da902bf0cd35f314115bc04dab0f3dd96fd729
David Herrera <herrera.dl@gmail.com>
2013-02-13 18:07:51 -0800

    Clean up extra JS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,26 +19,3 @@ function init() {
     }
   } )
 }
-
-/*
-function showInfo(data, tabletop) {
-
-  console.log(data);
-
-  var source = $("#entry-template").html();
-  var template = Handlebars.compile(source);
-
-  $.each( data, function(i) {
-    var content = {
-      title: data[i].title,
-      author: data[i].author,
-      theme: data[i].theme,
-      dateadded: data[i].dateadded
-    }
-    var html = template(content);
-    $("#table-body").append(html);
-  });
-
-}
-*/
-// window.onload = function() { init() };
